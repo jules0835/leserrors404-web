f8700a24093464d1786373733438989381b0effe
Jules G. <84841212+jules0835@users.noreply.github.com>
2024-09-24 15:40:20 +0200

    init

---
+++ src/app/layout.jsx
@@ -0,0 +1,18 @@
+import "./globals.css"
+
+export const metadata = {
+  title: "Cyna",
+  description: "Web App For Cyna",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
