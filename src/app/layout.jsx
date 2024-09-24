3d553856b0ea12860971e94e6c7d0c2e6dc3a64e
Jules G. <84841212+jules0835@users.noreply.github.com>
2024-09-24 15:56:56 +0200

    Update styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import "./globals.css"
+import "../styles/globals.css"
 
 export const metadata = {
   title: "Cyna",
@@ -8,11 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   )
 }
